a5fba132a147a397f280d0f44f0751cf4759ca77
josiahjswab <josiahjswab@gmail.com>
2021-05-05 12:15:06 -0700

    movie cache functioning

--- dnc-300-movie-finder-data/Controllers/MovieFinderController.cs
+++ dnc-300-movie-finder-data/Controllers/MovieFinderController.cs
@@ -6,26 +6,60 @@ using System.Web.Mvc;
 using System.Net;
 using System.IO;
 using System.Net.Http;
+using System.Runtime.CompilerServices;
 using System.Security.Cryptography;
 using dnc_300_movie_finder_data.Models;
+using Newtonsoft.Json;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Web.Hosting;
+using dnc_300_movie_finder_data.Helpers;
 
 namespace dnc_300_movie_finder_data.Controllers
 {
-    public class MovieFinderController : Controller
 
+    public class MovieFinderController : Controller
     {
+        
+        string filePath = HostingEnvironment.MapPath("~/dataa");
+
+        private List<Movie> MoviesCacheTransferList = new List<Movie>();
+
+        private DataSerializer dataSerializer = new DataSerializer();
         public ActionResult FindMovie()
         {
-            Console.WriteLine("Something");
-            //var movie = new Movie() { Title = "101 dalmations" };
             return View();
         }
 
         //[HttpPost]
         public ActionResult MovieResults(string searchString)
         {
+            List<Movie> MoviesCache = dataSerializer.BinaryDeserialize(filePath) as List<Movie>;
+            if (MoviesCache != null)
+            {
+                foreach (var movie in MoviesCache)
+                {
+                    if (searchString == movie.Title)
+                    {
+                        return View(movie);
+                    }
+                    else
+                    {
+                        MoviesCacheTransferList.Add(movie);
+                    }
+                }
+            }
+
             string response = CallOmdb($"http://www.omdbapi.com/?t={searchString}&apikey=af11b31");
-            return Content(response);
+
+            Movie dP = JsonConvert.DeserializeObject<Movie>(response);
+
+            MoviesCacheTransferList.Add(dP);
+
+            dataSerializer.BinarySerialize(MoviesCacheTransferList, filePath);
+
+            List<Movie> foo = dataSerializer.BinaryDeserialize(filePath) as List<Movie>;
+
+            return View(dP);
         }
 
         public static string CallOmdb(string url)
---
+++ dnc-300-movie-finder-data/Helpers/DataSerializer.cs
@@ -0,0 +1,38 @@
+﻿using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using Microsoft.Ajax.Utilities;
+using System.Web;
+
+namespace dnc_300_movie_finder_data.Helpers
+{
+    public class DataSerializer
+    {
+        public void BinarySerialize(object data, string filePath)
+        {
+            FileStream fileStream;
+            BinaryFormatter bf = new BinaryFormatter();
+
+            if(File.Exists(filePath)) File.Delete(filePath);
+            fileStream = File.Create(filePath);
+            bf.Serialize(fileStream, data);
+            fileStream.Close();
+        }
+
+        public object BinaryDeserialize(string filePath)
+        {
+            object obj = null;
+            FileStream fileStream;
+            BinaryFormatter bf = new BinaryFormatter();
+
+            if (File.Exists(filePath))
+            {
+                fileStream = File.OpenRead(filePath);
+                obj = bf.Deserialize(fileStream);
+                fileStream.Close();
+            }
+
+            return obj;
+        }
+    }
+
+}
